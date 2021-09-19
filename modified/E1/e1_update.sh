c9e246ae861399f5b0e2687de09cd4fb1eae78d6
niceboy <niceboygithub@github.com>
2021-09-19 11:46:00 +0800

    Add Aqara E1 Hub 3.1.5_0020

--- modified/E1/e1_update.sh
+++ modified/E1/e1_update.sh
@@ -8,15 +8,15 @@ fi
 cd /tmp
 
 echo "Updating Coor"
-/tmp/curl -s -k -L -o /tmp/coor.bin https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/original/E1/3.1.3_0066/Network-Co-Processor.ota
+/tmp/curl -s -k -L -o /tmp/coor.bin https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/original/E1/3.1.5_0020/Network-Co-Processor.ota
 [ "$(md5sum /tmp/coor.bin)" = "59b527769c2ecb2b840967f97b88eaa3  /tmp/coor.bin" ] && zigbee_msnger zgb_ota /tmp/coor.bin
 
 echo "Updating linux kernel"
-/tmp/curl -s -k -L -o /tmp/kernel https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/original/E1/3.1.3_0066/kernel_3.1.3_0066
-[ "$(md5sum /tmp/kernel)" = "c5fb24da4d0ba28181a1c0c7f71e5497  /tmp/kernel" ] && fw_update.sh /tmp/kernel
+/tmp/curl -s -k -L -o /tmp/kernel https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/original/E1/3.1.5_0020/kernel_3.1.5_0020
+[ "$(md5sum /tmp/kernel)" = "d9b0e0c505bc675543a3400ee2e99103  /tmp/kernel" ] && fw_update.sh /tmp/kernel
 echo 3 >/proc/sys/vm/drop_caches; sleep 1; sync
 
 echo "Update root file system"
-/tmp/curl -s -k -L -o /tmp/rootfs.sqfs https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/modified/E1/3.1.3_0066/rootfs_3.1.3_0066_modified.sqfs
-[ "$(md5sum /tmp/rootfs.sqfs)" = "2a8c23fa8e0f742269f0eb1dc13bbc7c  /tmp/rootfs.sqfs" ] && fw_update.sh /tmp/rootfs.sqfs
+/tmp/curl -s -k -L -o /tmp/rootfs.sqfs https://raw.githubusercontent.com/niceboygithub/AqaraCameraHubfw/main/modified/E1/3.1.5_0020/rootfs_3.1.5_0020_modified.sqfs
+[ "$(md5sum /tmp/rootfs.sqfs)" = "0a7483434693cc822de8b29b345e51c6  /tmp/rootfs.sqfs" ] && fw_update.sh /tmp/rootfs.sqfs
 sync; sync
